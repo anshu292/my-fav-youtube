154ea4a2a8788abcba42153f69b690e6c902ef5a
Anshu Gupta <86077759+anshu292@users.noreply.github.com>
2023-07-08 23:16:27 +0530

    Update CommentsContainer.js

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -113,7 +113,7 @@ const CommentsContainer = () =>
 {
     return (
     <div className='m-5 p-2'>
-        <h1 className="text-2xl font-bold"> Comments Container: </h1>
+        <h1 className="text-2xl font-bold"> Comments: </h1>
        <CommentsList comments={commentsData}/>
         </div>
     );
